db5a71d0aab9fcdc1824a59f87b15a3fed255672
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-17 11:49:50 -0700

    Size returns number of objects in storage

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,3 +1,6 @@
+//const _ = require('../../../lib/underscore/underscore');
+//var _ = require('/c/Users/Paul Dockery/dev/hack-reactor/junior/hrsf78-data-structures/lib/underscore');
+
 var Stack = function() {
   var someInstance = {};
 
@@ -12,7 +15,22 @@ var Stack = function() {
   };
 
   someInstance.size = function() {
+  //return objLength(storage);
+    let count = 0;
+    for (prop in storage) {
+      if (storage.hasOwnProperty(prop)) {
+        count++;
+      }
+    }
+    return count;
   };
 
   return someInstance;
+};
+
+
+var objLength = function(obj) {
+  let count = 0;
+  _.each(obj, () => count++);
+  return count;
 };
