b456daa30197d5da69dacf7dba7c23e112ca4e30
Hack Reactor Student <student@hackreactor.com>
2017-05-17 15:09:10 -0700

    Functional instantiation queue complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,12 +3,27 @@ var Queue = function() {
   var storage = {};
   var queueSize = 0;
 
-  // Implement the methods below
+  someInstance.shift = function() {
+    for (let i = 1; i <= queueSize; i++) {
+      if (storage[i + 1]) {
+        storage[i] = storage[i + 1];
+      }
+    }
+    delete storage[queueSize];
+  };
 
   someInstance.enqueue = function(value) {
+    queueSize++;
+    storage[queueSize] = value;
   };
 
   someInstance.dequeue = function() {
+    if (queueSize > 0) {
+      let dequeuedItem = storage[1];
+      someInstance.shift();
+      queueSize--;
+      return dequeuedItem;
+    }
   };
 
   someInstance.size = function() {
