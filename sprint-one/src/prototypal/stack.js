330b848ff965355b7e493613e9614eddc2d20a25
Hack Reactor Student <student@hackreactor.com>
2017-05-17 20:35:28 -0700

    prototypal passes except doesn't extend constructor's prototype

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackModel = Object.create(Stack.prototype);
+  stackModel.storage = {};
+  stackModel.stackSize = 0;
+  return stackModel;
 };
 
 var stackMethods = {};
 
+Stack.prototype.push = function(value) {
+  this.stackSize++;
+  this.storage[this.stackSize] = value;
+};
 
+Stack.prototype.size = function() {
+  return this.stackSize;
+};
+
+Stack.prototype.pop = function() {
+  if (this.stackSize > 0) {
+    let poppedItem = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    this.stackSize--;
+    return poppedItem;
+  }
+};
