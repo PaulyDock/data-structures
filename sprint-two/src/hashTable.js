2e629409b2a1aeb5ffec09782a53ae447141a32f
Hack Reactor Student <student@hackreactor.com>
2017-05-19 16:10:53 -0700

    Basic hashtable impl

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,40 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var formerTuples = this.retrieve(k);
+  console.log('inserting ', formerTuples, index, [k, v]);
+  if (!formerTuples) {  
+    this._storage.set(index, [[k, v]]);
+  } else {
+    formerTuples.push([k, v]);
+    this._storage.set(index, formerTuples);
+  }
+  console.log('get after insert', this._storage.get(index));
+  //console.log('inserting ', formerTuples, index, [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var retrievedBucket = this._storage.get(index);
+  console.log('retrieving ', index, retrievedBucket);
+  if (retrievedBucket) {
+    for (let i = 0; i < retrievedBucket.length; i++) {
+      if (retrievedBucket[i][0] === k) {
+        console.log('retrieved ', retrievedBucket[i][1]);
+        return retrievedBucket[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
+};
+
+HashTable.prototype.tupleLookup = function(v) {
+  
+
 };
 
 
@@ -23,4 +49,10 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+/*
+  limitedArray.each = function(callback) {
+    for (var i = 0; i < storage.length; i++) {
+      callback(storage[i], i, storage);
+    }
+  };
+*/
