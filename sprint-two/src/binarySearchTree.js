3f70a54d60275f2a2517a204682e15f66aa9a416
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-20 08:39:32 -0700

    binarySearchTree cleanup, incomplete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,31 +1,43 @@
 var BinarySearchTree = function(value) {
   this.left = {};
   this.right = {};
+  this.nodeValue = value;
 };
 
-
 BinarySearchTree.prototype.insert = function(value) {
-  if (this.nodeValue) {
-    if (value > this.nodeValue) {
-      this.insert(this.right);
+  console.log('pre-insert - ', this.left, this.right);
+  if (value > this.nodeValue) {
+    if (this.right.nodeValue === undefined) {
+      this.right = new BinarySearchTree(value);
     } else {
-      this.insert(this.left);
+      this.right.insert(value);
     }
   } else {
+    if (this.left.nodeValue === undefined) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
   }
 };
 
-
 BinarySearchTree.prototype.contains = function(value) {
-
+  if (this.nodeValue === undefined) {
+    return false;
+  } else if (this.nodeValue === value) {
+    return true;
+  } else if (value > this.nodeValue) {
+    return this.right.contains(value);
+  } else {
+    return this.left.contains(value);
+  }
 };
-
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
-
 };
 
-
+//BinaryTree = new BinarySearchTree(5);
+//console.log(BinaryTree.contains(5));
 
 
 
