3e0c28e434eb33e2e1a94177eee468275e765e7d
Hack Reactor Student <student@hackreactor.com>
2017-05-20 11:29:40 -0700

    binarySearchTree passes all tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,39 +1,58 @@
 var BinarySearchTree = function(value) {
-  this.left = {};
-  this.right = {};
-  this.nodeValue = value;
+  var binarySearchTree = Object.create(binarySearchTreePrototype);
+
+  binarySearchTree.left = null;
+  binarySearchTree.right = null;
+  binarySearchTree.value = value;
+
+  return binarySearchTree;
 };
 
-BinarySearchTree.prototype.insert = function(value) {
-  console.log('pre-insert - ', this.left, this.right);
-  if (value > this.nodeValue) {
-    if (this.right.nodeValue === undefined) {
-      this.right = new BinarySearchTree(value);
+var binarySearchTreePrototype = {};
+
+binarySearchTreePrototype.insert = function(value) {
+  //console.log('pre-insert - ', this.left, this.right);
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
     } else {
       this.right.insert(value);
     }
   } else {
-    if (this.left.nodeValue === undefined) {
-      this.left = new BinarySearchTree(value);
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
     } else {
       this.left.insert(value);
     }
   }
 };
 
-BinarySearchTree.prototype.contains = function(value) {
-  if (this.nodeValue === undefined) {
-    return false;
-  } else if (this.nodeValue === value) {
+binarySearchTreePrototype.contains = function(value) {
+  if (this.value === value) {
     return true;
-  } else if (value > this.nodeValue) {
-    return this.right.contains(value);
+  } else if (value > this.value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
   } else {
-    return this.left.contains(value);
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
   }
 };
 
-BinarySearchTree.prototype.depthFirstLog = function(callback) {
+binarySearchTreePrototype.depthFirstLog = function(callback) {
+  callback(this.value);  
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
 };
 
 //BinaryTree = new BinarySearchTree(5);
